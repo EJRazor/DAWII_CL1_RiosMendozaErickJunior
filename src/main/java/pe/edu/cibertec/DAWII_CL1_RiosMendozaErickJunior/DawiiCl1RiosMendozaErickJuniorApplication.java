36a94e5e2557d7026bf14cf79b73a2eb97360b35
EJRazor <erickju01@gmail.com>
2023-05-07 07:09:42 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL1_RiosMendozaErickJunior/DawiiCl1RiosMendozaErickJuniorApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL1_RiosMendozaErickJunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1RiosMendozaErickJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1RiosMendozaErickJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_CL1_RiosMendozaErickJunior/DawiiCl1RiosMendozaErickJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL1_RiosMendozaErickJunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl1RiosMendozaErickJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
